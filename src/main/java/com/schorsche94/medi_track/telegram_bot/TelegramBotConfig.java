c256496d8b7991fde1d13a545bc847c3875f5b12
schorsche94 <gcasandra94@gmail.com>
2026-06-18 20:23:50 +0300

    MTB#002: Add medication menu in TGBot

---
+++ src/main/java/com/schorsche94/medi_track/telegram_bot/TelegramBotConfig.java
@@ -0,0 +1,17 @@
+package com.schorsche94.medi_track.telegram_bot;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+@Configuration
+public class TelegramBotConfig {
+
+//    @Bean
+//    public TelegramClient telegramClient(@Value("${telegram.bot.token}") String botToken) {
+//        return new OkHttpTelegramClient(botToken);
+//    }
+}
